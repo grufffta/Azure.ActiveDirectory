8a9f1bb3f6c7292041444fedd24f21e46dc18918
Gareth Barlow <gareth@distechsw.co.uk>
2014-04-02 22:48:51 +0100

    Added Sample

---
+++ Azure.ActiveDirectory/Sample/RolesBasedAuthenticationSample/RolesBasedAuthenticationSample/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Permissions;
+using System.Web.Mvc;
+using Azure.ActiveDirectory;
+
+namespace RolesBasedAuthenticationSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IActiveDirectoryGraphClient _graphClient = new GraphClient();
+
+        [Authorize]
+        public ActionResult UserProfile()
+        {
+            return View(_graphClient.CurrentUser);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role = "Admin")]
+        public ActionResult Admin()
+        {
+            ViewBag.Message = "Your admin page.";
+
+            return View();
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role="Member")]
+        public ActionResult Member()
+        {
+            ViewBag.Message = "Your member page.";
+
+            return View();
+        }
+    }
+}
